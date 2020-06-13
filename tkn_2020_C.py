a10b20f44444e9cb47e3cf79e49563d5a56abd3e
Yuki Kubota <yuki.kubota.0223@gmail.com>
2020-06-14 00:07:44 +0900

    C問題、解法着眼点あっていたがリストズレでWA。直してもTLE消えず

--- tkn_2020_C.py
+++ tkn_2020_C.py
@@ -2,19 +2,19 @@ def conv(A,N):
     #A(座標iの電灯の強さリスト)からB(座標iを照らしている電灯数)への変換
     B=[0 for n in range(N)]
     for n in range(1,N+1):
-        il_min=max(int(n-A[n-1]-0.5)+1,0)
+        il_min=max(int(n-A[n-1]-0.5)+1,1)
         il_max=min(int(n+A[n-1]+0.5),N)
-        B_=[B[i]+1 for i in range(N) if il_min<=i<=il_max]
-    return B_
+        for i in range(il_min,il_max+1):
+            B[i-1]+=1
+    return B
 
 def main():
     #input
     N,K=map(int,input().strip().split())
     A=list(map(int,input().strip().split()))
 
-    if K>30:
+    if K>45:
         A=[N for n in range(N)]
-        A[-1]=2*N
     else:
         for k in range(K):
             B=conv(A,N)
@@ -22,6 +22,7 @@ def main():
                 break
             else:
                 A=B
+            print(B)
     return " ".join(list(map(str,A)))
 
 if __name__=="__main__":
