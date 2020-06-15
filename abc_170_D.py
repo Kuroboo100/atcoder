b79dee1cfb24cba842628ea320a43ffeec879d71
Yuki Kubota <yuki.kubota.0223@gmail.com>
2020-06-15 22:47:31 +0900

    abc_170_D - Not Divisible AC

--- abc_170_D.py
+++ abc_170_D.py
@@ -1,67 +1,40 @@
-import math
-
-def prime(x):
-    if x==1:
-        prime=True
-        #この問題に合わせて１でもTrueとする
-    else:
-        prime=True
-        for i in range(2,int(math.sqrt(x)+1)):
-            if x%i==0:
-                prime=False
-                break
-    return prime
-
 def main():
+    """
+    要素数Amaxのbool配列lを生成し、初期値を全てTrueとする。
+    Aを昇順にソートし、Aの要素を順に見て、要素の倍数インデックスを全てFalseにする、
+    という操作を繰り返す。
+    あるp=A[n]を見た時にl[p]==Trueの場合、A[n]は全てのA[j](j<n)で割り切れない。
+    ※A[j]<A[n]の全ての倍数のいずれともA[n]が一致しないということは、A[n]はA[j]で割り切れないことを意味します。
+    A[n]==A[n+1]の時にA[n]はA[n+1]で割り切れるが、この場合を除いてA[n]は任意のA[i]で
+    割り切れないのでカウントを一つインクリメントする。
+    Aの全ての要素について操作を完了した時のカウント値が答えとなる。
+    """
     #input
     N=int(input())
     A=list(map(int,input().strip().split()))
-
-    A.sort(reverse=True)
+    #昇順
+    A.sort()
+    #AmaxまでのTrue配列
+    l=[True for _ in range(A[-1]+1)]
     
-    #割り切れた場合+1
     cnt=0
-    for i in range(N-1):
-        #A[i]が素数または1の場合
-        if prime(A[i]):
-            #前後に同じ数がない場合、割り切れない
-            if i<N-1:
-                if A[i]==A[i+1]:
-                    cnt+=1
-            elif i>0:
-                if A[i]==A[i-1]:
-                    cnt+=1
-        #A[i]が素数ではない場合
-        else:
-            for j in reversed(range(i+1,N)):
-                if A[i]%A[j]==0:
-                    cnt+=1
-                    break
-    if A[-1]%A[-2]==0:
+    for n in range(N-1):
+        if l[A[n]]==True:
+            if A[n]==A[n+1]:
+                l[A[n]]=False
+            else:
+                l[A[n]]=False
+                cnt+=1
+            i=1
+            while i*A[n]<=A[-1]:
+                l[A[n]*i]=False
+                i+=1
+    #最終項はn+1と比較できないので別で考える。
+    if l[A[N-1]]==True:
         cnt+=1
-    print(N-cnt)
+        l[A[N-1]]=False
+    return cnt
 
 if __name__=="__main__":
-    main()
-
-s
-N=int(input())
-A=list(map(int,input().strip().split()))
-
-A.sort()
-cnt=0
-div=[]
-for n in range(1,N):
-    for i in range(len(div)):
-        if div==[]:
-            for m in range(n-1):
-                if A[n]%A[m]==0:
-                    cnt+=1
-                    div.append(A[m])
-        else:
-            for i in range(len(div)):
-                if A[n]%div[i]==0:
-                    cnt+=1
-                    div.append(A[m])
-print(N-cnt)
-
+    print(main())
+    
