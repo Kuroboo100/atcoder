abc904156e9fa857d39a021f16ac0214919bb448
Yuki Kubota <yuki.kubota.0223@gmail.com>
2020-06-27 16:11:37 +0900

    abc_159_D インデックスを修正しAC

--- abc_159_D.py
+++ abc_159_D.py
@@ -26,16 +26,16 @@ k=1,2,...,N
 N=int(input())
 A=list(map(int,input().strip().split()))
 
-l=[0 for n in range(N+1)]
+l=[0 for n in range(N)]
 
 for e in A:
-    l[e]+=1
+    l[e-1]+=1
 
 count=0
 for n in range(N):
     if l[n]>=2:
         count+=l[n]*(l[n]-1)//2
             
-for n in range(1,N+1):
-    print(count-l[A[n-1]]+1)
+for n in range(N):
+    print(count-l[A[n]-1]+1)
 
