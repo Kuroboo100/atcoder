eeb426a92eda000de611e80671b5b67b73a3ad85
Yuki Kubota <yuki.kubota.0223@gmail.com>
2020-07-04 23:13:58 +0900

    abc_131_D_Megalomania 解法通り

---
+++ abc_131_D.py
@@ -0,0 +1,24 @@
+N=int(input())
+A=[]
+B=[]
+for n in range(N):
+    a,b=map(int,input().strip().split())
+    A.append(a)
+    B.append((b,n))
+
+B=sorted(B)
+
+sumA=0
+fin=True
+for n in range(N):
+    tmp=B[n][1]
+    sumA+=A[tmp]
+    print(sumA,tmp,B[n][0])
+    if sumA>B[n][0]:
+        fin=False
+        break
+
+if fin==True:
+    print("Yes")
+else:
+    print("No")
